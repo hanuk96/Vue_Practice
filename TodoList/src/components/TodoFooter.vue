d65575a576097b7f2065be979699cc80c86661f0
hanuk <gi7182@naver.com>
2021-09-26 09:59:27 +0900

    todolist

---
+++ TodoList/src/components/TodoFooter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <span> <i class="far fa-trash-alt" v-on:click="clearTodo"></i></span>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {
+      sampleData: "",
+    };
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdated() {},
+  updated() {},
+  beforeUnmount() {},
+  unmounted() {},
+  methods: {
+    clearTodo() {
+      localStorage.clear();
+    },
+  },
+};
+</script>
---
+++ TodoList/src/components/TodoHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <h1>TODO List</h1>
+  </header>
+</template>
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {
+      sampleData: "",
+    };
+  },
+  methods: {},
+};
+</script>
+<style scoped>
+h1 {
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
